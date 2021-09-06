f5367e8d0f6a70ad6a2fb002c56310f9704e58e2
adammabin <adam.mabin@gmail.com>
2021-09-06 17:58:06 +0100

    Use Random object instead of math.random

--- Source/Array/Shuffle1D.lua
+++ Source/Array/Shuffle1D.lua
@@ -1,5 +1,5 @@
 local function Shuffle1D(Array, Seed)
-    math.randomseed(Seed or os.clock())
+    local Generator = Random.new(Seed or os.clock())
 
     local ArraySize = #Array
     local Result = table.create(ArraySize)
@@ -9,7 +9,7 @@ local function Shuffle1D(Array, Seed)
     end
 
     for Index = 1, ArraySize do
-        local Generated = math.random(1, ArraySize)
+        local Generated = Generator:NextInteger(1, ArraySize)
         Result[Index], Result[Generated] = Result[Generated], Result[Index]
     end
 
