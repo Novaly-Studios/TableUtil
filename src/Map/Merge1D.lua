65d2c2dfa579b0dfd2bbd13921500b96369958e3
tpc9000 <adam.mabin@googlemail.com>
2022-05-12 23:05:04 +0100

    Optimize Map.Merge1D

--- src/Map/Merge1D.lua
+++ src/Map/Merge1D.lua
@@ -1,10 +1,9 @@
 --- Merges various tables together, into a union data type.
 local function Merge1D(...)
     local Result = {}
-    local Args = {...}
 
-    for Index = 1, #Args do
-        for Key, Value in pairs(Args[Index]) do
+    for Index = 1, select("#", ...) do
+        for Key, Value in pairs(select(Index, ...)) do
             Result[Key] = Value
         end
     end
