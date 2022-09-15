3891eb2d65c4d97c9fff1de0df155992d03bcc9b
adammabin <adam.mabin@gmail.com>
2022-09-15 09:18:41 +0100

    Fix module require links, add todo comment

--- src/Map/CreateNonOverwritingPatchDeep.lua
+++ src/Map/CreateNonOverwritingPatchDeep.lua
@@ -2,6 +2,7 @@
 --- This allows us to apply an additional merge to add new fields to values which were not originally nil.
 --- Good use case: want to merge in new default fields to a player's data structure without overwriting existing fields.
 --- @todo Return nil if result is empty & wrap top level with another function? That way we trim recursive merge work for the resulting empty tables, which will be the common use case.
+--- @todo Deep copy for intuitiveness.
 local TYPE_TABLE = "table"
 
 local function CreateNonOverwritingPatchDeep(Previous, Template)
--- src/Map/init.lua
+++ src/Map/init.lua
@@ -1,7 +1,12 @@
 return {
+    CreateNonOverwritingPatchDeep = require(script:WaitForChild("CreateNonOverwritingPatchDeep"));
+    ToKeyValueArray1D = require(script:WaitForChild("ToKeyValueArray1D"));
+    CloneDeep = require(script:WaitForChild("CloneDeep"));
     MergeDeep = require(script:WaitForChild("MergeDeep"));
+    Equals1D = require(script:WaitForChild("Equals1D"));
     Filter1D = require(script:WaitForChild("Filter1D"));
     Values1D = require(script:WaitForChild("Values1D"));
+    Clone1D = require(script:WaitForChild("Clone1D"));
     Merge1D = require(script:WaitForChild("Merge1D"));
     Keys1D = require(script:WaitForChild("Keys1D"));
     Map1D = require(script:WaitForChild("Map1D"));
