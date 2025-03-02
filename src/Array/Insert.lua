04e79000236f9311b6e41858fae1061ea20c903b
tpc9000 <adam.mabin@gmail.com>
2025-03-02 00:36:30 +0000

    Insert default param

--- src/Array/Insert.lua
+++ src/Array/Insert.lua
@@ -5,7 +5,8 @@
 --- Inserts a value into a new array with an optional "insert at" index.
 local function Insert<T>(Array: {T}, Value: T?, At: number?): {T}
     local NewSize = #Array + 1
-    
+    At = At or NewSize
+
     if (Value == nil and At == NewSize) then
         return Array
     end
